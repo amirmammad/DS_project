06c0a57198f39f1f837ef9a4092e806bd64eee09
MaMaDDMD <M.H.damad@chmail.ir>
2023-12-25 12:51:09 +0330

    tester fixed

--- main_mh.py
+++ main_mh.py
@@ -9,17 +9,28 @@ from difflib import SequenceMatcher
 from collections import Counter
 from whoosh.analysis import SimpleAnalyzer, StopFilter
 
-#static methods
-#tokenizing
-def tokenize(text):
-    analyzer = SimpleAnalyzer() | StopFilter()
-    tokens = [token.text for token in analyzer(text)]
-    return tokens
+#class for static methods and fields
+class Static:
+    #tokenizing
+    @staticmethod
+    def tokenize(text):
+        analyzer = SimpleAnalyzer() | StopFilter()
+        tokens = [token.text for token in analyzer(text)]
+        return tokens
 
-#static fields
-query = str()
-dim = set()
-docs_text = dict()
+    #giving input and run the program
+    @staticmethod
+    def InputGir(Query, Doc_List):
+        query = Query
+        Static.dim = set(Static.tokenize(query))
+        for doc_num in Doc_List:
+            Static.docs_text[doc_num] = open(os.getcwd() + "\\data\\document_" + str(doc_num) + ".txt", "r", encoding='utf-8').read()
+        system = Program(query, Doc_List)
+
+    query = str()
+    dim = set()
+    docs_text = dict()
+    most_related_doc_number = int()
 
 #class for calculating vector of each paragraph of each document
 class Line_1:
@@ -31,7 +42,7 @@ class Line_1:
     #building the vector of the paragraph
     def build_line_1_vector(self):
         tf_idf = dict()
-        for term in dim:
+        for term in Static.dim:
             tf_idf[term] = self.tf[term] * self.idf[term]
         return tf_idf
 
@@ -40,7 +51,7 @@ class Document:
     def __init__(self, doc_text):
         self.doc_text = doc_text
         self.doc_lines_idf = self.line_1_idf_calculator(doc_text)
-        self.doc_lines_tf = self.line_1_tf_calculator(tokenize(doc_text))
+        self.doc_lines_tf = self.line_1_tf_calculator(Static.tokenize(doc_text))
         self.line_vector_list = list()
         for line in doc_text.split("\n"):
             self.line_vector_list.append(Line_1(self.doc_lines_idf, self.doc_lines_tf))
@@ -50,12 +61,12 @@ class Document:
     def line_1_idf_calculator(self, doc_text):
         term_counter = Counter()
         doc_lines = doc_text.split("\n")
-        for term in dim:
+        for term in Static.dim:
             for line in doc_lines:
-                if term in tokenize(line):
+                if term in Static.tokenize(line):
                     term_counter[term] += 1
         idf = dict()
-        for term in dim:
+        for term in Static.dim:
             if term_counter[term] == 0:
                 idf[term] = log((len(doc_lines)) / 0.001)
             else:
@@ -65,10 +76,10 @@ class Document:
     #calculating tf of vector of each paragraph of each document in range of all of the words of the document of that paragraph
     def line_1_tf_calculator(self, tokenized_doc_text):
         term_counter = Counter()
-        for term in dim:
+        for term in Static.dim:
             term_counter[term] = 0
         for term in tokenized_doc_text:
-            if term in dim:
+            if term in Static.dim:
                 term_counter[term] += 1
         tf = dict()
         for term in term_counter:
@@ -79,7 +90,7 @@ class Document:
     def sum_line_vectors(self):
         doc_vector = Counter()
         for line in self.line_vector_list:
-            for term in dim:
+            for term in Static.dim:
                 doc_vector[term] += line.vector[term]
         doc_vector_dict = dict()
         for term in doc_vector:
@@ -96,15 +107,15 @@ class Query_1:
     #calculating idf of vector of query_1 in range of all of the documents of testcase
     def query_1_idf_calculator(self, doc_list):
         doc_counter = Counter()
-        for term in dim:
+        for term in Static.dim:
             for doc_num in doc_list:
-                tokenized_doc_text = tokenize(docs_text[doc_num])
+                tokenized_doc_text = Static.tokenize(Static.docs_text[doc_num])
                 if term in tokenized_doc_text:
                     doc_counter[term] += 1
         idf = dict()
-        for term in dim:
+        for term in Static.dim:
             if doc_counter[term] == 0:
-                idf[term] = log((len(doc_list)) / 1)
+                idf[term] = log((len(doc_list)) / 0.001)
             else:
                 idf[term] = log(len(doc_list) / doc_counter[term])
         return idf
@@ -112,16 +123,16 @@ class Query_1:
     #calculating tf of vector of query_1 in range of all of the words of all of the documents of testcase
     def query_1_tf_calculator(self, doc_list):
         term_counter = Counter()
-        for term in dim:
+        for term in Static.dim:
             term_counter[term] = 0
         for doc_num in doc_list:
-            doc_words = tokenize(docs_text[doc_num])
+            doc_words = Static.tokenize(Static.docs_text[doc_num])
             for term in doc_words:
-                if term in dim:
+                if term in Static.dim:
                     term_counter[term] += 1
-        query_words = tokenize(query)
+        query_words = Static.tokenize(Static.query)
         for term in query_words:
-            if term in dim:
+            if term in Static.dim:
                 term_counter[term] += 1 
         tf = dict()
         for term in term_counter:
@@ -131,7 +142,7 @@ class Query_1:
     #building the vector of query_1
     def build_query_1_vector(self):
         tf_idf = dict()
-        for term in dim:
+        for term in Static.dim:
             tf_idf[term] = self.tf[term] * self.idf[term]
         return tf_idf
 
@@ -146,14 +157,14 @@ class Query_2:
     def query_2_idf_calculator(self, doc_text):
         term_counter = Counter()
         doc_lines = doc_text.split("\n")
-        for term in dim:
+        for term in Static.dim:
             for line in doc_lines:
-                if term in tokenize(line):
+                if term in Static.tokenize(line):
                     term_counter[term] += 1
         idf = dict()
-        for term in dim:
+        for term in Static.dim:
             if term_counter[term] == 0:
-                idf[term] = log((len(doc_lines)) / 1)
+                idf[term] = log((len(doc_lines)) / 0.001)
             else:
                 idf[term] = log(len(doc_lines) / term_counter[term])
         return idf
@@ -161,15 +172,15 @@ class Query_2:
     #calculating tf of vector of query_2 in range of all of the words of the most related document
     def query_2_tf_calculator(self, doc_text):
         term_counter = Counter()
-        for term in dim:
+        for term in Static.dim:
             term_counter[term] = 0
-        doc_words = tokenize(doc_text)
+        doc_words = Static.tokenize(doc_text)
         for term in doc_words:
-            if term in dim:
+            if term in Static.dim:
                 term_counter[term] += 1
-        query_words = tokenize(query)
+        query_words = Static.tokenize(Static.query)
         for term in query_words:
-            if term in dim:
+            if term in Static.dim:
                 term_counter[term] += 1
         tf = dict()
         for term in term_counter:
@@ -179,7 +190,7 @@ class Query_2:
     #building the vector of query_2
     def build_query_2_vector(self):
         tf_idf = dict()
-        for term in dim:
+        for term in Static.dim:
             tf_idf[term] = self.tf[term] * self.idf[term]
         return tf_idf
 
@@ -188,16 +199,16 @@ class Line_2:
     def __init__(self, idf, line_text):
         self.line_text = line_text
         self.idf = idf
-        self.tf = self.line_2_tf_calculator(tokenize(line_text))
+        self.tf = self.line_2_tf_calculator(Static.tokenize(line_text))
         self.vector = self.build_line_2_vector()
 
     #calculating tf of vector of each paragraph of the most related document in range of all of the words of that paragraph
     def line_2_tf_calculator(self, line_words):
         term_counter = Counter()
-        for term in dim:
+        for term in Static.dim:
             term_counter[term] = 0
         for term in line_words:
-            if term in dim:
+            if term in Static.dim:
                 term_counter[term] += 1
         tf = dict()
         for term in term_counter:
@@ -207,24 +218,20 @@ class Line_2:
     #building the vector of the paragraph
     def build_line_2_vector(self):
         tf_idf = dict()
-        for term in dim:
+        for term in Static.dim:
             tf_idf[term] = self.tf[term] * self.idf[term]
         return tf_idf
 
 #class for running program
 class Program:
-    def __init__(self, Query, doc_list):
-        query = Query
-        dim = set(tokenize(query))
-        for doc_num in doc_list:
-            docs_text[doc_num] = open(os.getcwd() + "\\data\\document_" + str(doc_num) + ".txt", "r", encoding='utf-8').read()
+    def __init__(self, query, doc_list):
         self.query_1 = Query_1(doc_list)
         self.doc_dict = dict()
         for doc_num in doc_list:
-            self.doc_dict[doc_num] = Document(docs_text[doc_num])
-        self.most_related_doc_number = self.max_comparator(self.doc_dict, type="doc")
-        self.most_related_doc_text = self.doc_dict[self.most_related_doc_number].doc_text 
-        print(self.most_related_doc_number)
+            self.doc_dict[doc_num] = Document(Static.docs_text[doc_num])
+        Static.most_related_doc_number = self.max_comparator(self.doc_dict, type="doc")
+        self.most_related_doc_text = self.doc_dict[Static.most_related_doc_number].doc_text 
+        print(Static.most_related_doc_number)
         self.query_2 = Query_2(self.most_related_doc_text)
         self.most_related_doc_lines_vector_list = list()
         self.most_related_doc_lines_idf = self.line_2_idf_calculator(self.most_related_doc_text)
@@ -236,11 +243,11 @@ class Program:
     #calculating idf of vector of each paragraph of the most related document in range of all of the words of the most related document
     def line_2_idf_calculator(self, doc_text):
         term_counter = Counter()
-        for term in dim:
+        for term in Static.dim:
             term_counter[term] = 0
-        doc_words = tokenize(doc_text)
+        doc_words = Static.tokenize(doc_text)
         for term in doc_words:
-            if term in dim:
+            if term in Static.dim:
                 term_counter[term] += 1
         idf = dict()
         for term in term_counter:
@@ -255,9 +262,9 @@ class Program:
         similarity_score_dict = dict()
         for instance in search_domain:
             if type == "doc":
-                similarity_score_dict[instance] = 0.29 * (self.cosine_similarity(self.query_1.vector, search_domain[instance].doc_vector)) + 0.71 * (SequenceMatcher(None, query, search_domain[instance].doc_text).ratio())
+                similarity_score_dict[instance] = 0.4 * (self.cosine_similarity(self.query_1.vector, search_domain[instance].doc_vector)) + 0.6 * (SequenceMatcher(None, Static.query, search_domain[instance].doc_text).ratio())
             else:
-                similarity_score_dict[search_domain.index(instance)] = 0.84 * (self.cosine_similarity(self.query_2.vector, instance.vector)) + 0.16 * (SequenceMatcher(None, query, instance.line_text).ratio())
+                similarity_score_dict[search_domain.index(instance)] = 0.6 * (self.cosine_similarity(self.query_2.vector, instance.vector)) + 0.4 * (SequenceMatcher(None, Static.query, instance.line_text).ratio())
         similarity_score_list = sorted(similarity_score_dict.items(), key=lambda x: x[1], reverse=True)
         return similarity_score_list[0][0]
 
@@ -271,3 +278,4 @@ class Program:
             return 0.0
         similarity = dot_product / (magnitude1 * magnitude2)
         return similarity
+    
--- tester.py
+++ tester.py
@@ -3,19 +3,21 @@ from tqdm import tqdm
 import main_mh
 import os
 
-#u should change here
 file_path = os.getcwd() + "\\data.json"
-
 
 if __name__ == "__main__":
     with open(file_path, 'r') as file:
         data_list = json.load(file)
-    true_counter = 0
-    for data in tqdm(data_list, desc="Processing JSON data"):
-        #u should change here
-        system = main_mh.Program(data["query"], data["candidate_documents_id"])
-        if system.most_related_doc_number == int(data["document_id"]):
+    counter = 22246
+    true_counter = 15258
+    #data = tqdm(data_list, desc="Processing JSON data")
+    #for data in tqdm(data_list, desc="Processing JSON data"):
+    while counter != len(data_list):
+        main_mh.Static.InputGir(data_list[counter]["query"], data_list[counter]["candidate_documents_id"])
+        if main_mh.Static.most_related_doc_number == int(data_list[counter]["document_id"]):
             true_counter += 1
+        counter += 1
+        print(counter)
+        print(true_counter)
     true_percentage = (true_counter / len(data_list)) * 100
-    print(true_counter)
-    print()
+    print(true_percentage)
